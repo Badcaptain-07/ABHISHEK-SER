1221fe98963c1176e8e6ffd9580f8c1f4ed1ff70
Badcaptain-07 <107783496+Badcaptain-07@users.noreply.github.com>
2023-07-16 09:50:12 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['919074692450'] //ur owner number
-global.ownernomer = "919074692450" //ur owner number2
-global.ownername = "ABHISHEK SURESH"//ur owner name
+global.owner = ['919037858501'] //ur owner number
+global.ownernomer = "919037858501" //ur owner number2
+global.ownername = "ARUN"//ur owner name
 global.ytname = "YT: Comedy Melody CH" //ur yt chanel name
-global.socialm = "GitHub: AbhishekSuresh2" //ur github or insta name
-global.location = "India, Idukki, Kerala" //ur location
+global.socialm = "GitHub: Loopzser" //ur github or insta name
+global.location = "India, Palakkad, Kerala" //ur location
 
 //new
-global.botname = "ABHISHEK-SER"
-global.ownernumber = '919074692450'
-global.ownername = 'ABHISHEK SURESH'
-global.ownerNumber = ["919074692450@s.whatsapp.net"]
+global.botname = "LOOPZ-MD"
+global.ownernumber = '919037858501'
+global.ownername = 'ARUN'
+global.ownerNumber = ["919037858501@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@comedymelodych8468"
 global.websitex = "https://youtu.be/7FJBXptnUu4"
-global.wagc = "https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD"
+global.wagc = "https://chat.whatsapp.com/BOLb0ICN3AJ5dloRBw5VD"
 global.themeemoji = '🎐'
 global.wm = "ABHISHEK-SER Inc."
 global.botscript = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' //script link
 global.packname = "ABHISHEK-SER👻ABHISHEK SURESH"
 global.author = "ABHISHEK SURESH\n\n919074692450"
-global.creator = "919074692450@s.whatsapp.net"
+global.creator = "919037858501@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
